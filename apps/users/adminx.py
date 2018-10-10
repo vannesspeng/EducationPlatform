692e63e0f83b98cea313269596141aea179f7c55
vannesspeng1990 <401041628@qq.com>
2018-10-10 17:13:31 +0800

    配置了页头页脚信息，修改了菜单的顺序，配置apps中文名

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # author:pyy
 # datetime:2018/10/10 10:01
+from django.contrib.auth.models import Group, Permission
 
 import xadmin
+from courses.models import Course, Lesson, Video, CourseResource
+from operation.models import CourseComments, UserCourse, UserFavorite, UserMessage, UserAsk
+from organization.models import CityDict, Teacher, CourseOrg
+from xadmin import views
+from xadmin.models import Log
+
+from .models import EmailVerifyRecord, Banner, UserProfile
 
 
-from .models import EmailVerifyRecord, Banner
-
-
-#创建admin的管理类，不再是集成admin，而是集成object
+# 创建admin的管理类，不再是集成admin，而是集成object
 class EmailVerifyRecordAdmin(object):
     list_display = ['code', 'email', 'send_type', 'send_time']
     # 配置搜索字段,不做时间搜索
@@ -17,10 +22,58 @@ class EmailVerifyRecordAdmin(object):
     # 配置筛选字段
     list_filter = ['code', 'email', 'send_type', 'send_time']
 
+
 class BannerAdmin(object):
     list_display = ['title', 'image', 'url', 'index', 'add_time']
     search_fields = ['title', 'image', 'url', 'index']
     list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
+
+# X admin的全局配置信息设置
+class BaseSetting(object):
+    # 主题功能开启
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "i爱习后台管理"
+    site_footer = "isdutynet.top"
+    # 收起菜单
+    menu_style = "accordion"
+
+    def get_site_menu(self):
+        return (
+            {'title': '课程管理', 'menus': (
+                {'title': '课程信息', 'url': self.get_model_url(Course, 'changelist')},
+                {'title': '章节信息', 'url': self.get_model_url(Lesson, 'changelist')},
+                {'title': '视频信息', 'url': self.get_model_url(Video, 'changelist')},
+                {'title': '课程资源', 'url': self.get_model_url(CourseResource, 'changelist')},
+                {'title': '课程评论', 'url': self.get_model_url(CourseComments, 'changelist')},
+                )
+            },
+            {'title': '机构管理', 'menus': (
+                {'title': '所在城市', 'url': self.get_model_url(CityDict, 'changelist')},
+                {'title': '机构讲师', 'url': self.get_model_url(Teacher, 'changelist')},
+                {'title': '机构信息', 'url': self.get_model_url(CourseOrg, 'changelist')},
+            )},
+            {'title': '用户管理', 'menus': (
+                {'title': '用户信息', 'url': self.get_model_url(UserProfile, 'changelist')},
+                {'title': '用户验证', 'url': self.get_model_url(EmailVerifyRecord, 'changelist')},
+                {'title': '用户课程', 'url': self.get_model_url(UserCourse, 'changelist')},
+                {'title': '用户收藏', 'url': self.get_model_url(UserFavorite, 'changelist')},
+                {'title': '用户消息', 'url': self.get_model_url(UserMessage, 'changelist')},
+            )},
+            {'title': '系统管理', 'menus': (
+                {'title': '用户咨询', 'url': self.get_model_url(UserAsk, 'changelist')},
+                {'title': '首页轮播', 'url': self.get_model_url(Banner, 'changelist')},
+                {'title': '用户分组', 'url': self.get_model_url(Group, 'changelist')},
+                {'title': '用户权限', 'url': self.get_model_url(Permission, 'changelist')},
+                {'title': '日志记录', 'url': self.get_model_url(Log, 'changelist')},
+            )},
+        )
+
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
